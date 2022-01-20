43890c52c2c8939efaa804d119cde01f696a6143
yarovik1982 <yar.oleg2018@yandex.ru>
2022-01-20 13:47:55 +0300

    miniChange

--- js/modules/InitMolal.js
+++ js/modules/InitMolal.js
@@ -14,17 +14,23 @@ function InitModal(){
       element.classList.remove(_class);
    }
 
+   function disableScroll(){
+      window.scrollTo(0,0);
+   }
+
    document.addEventListener("click", function(event){
       let elem = event.target;
       if(elem.closest('[data-id=book]')){
          
          addClassElement(body, "lock");
          addClassElement(modal, "visible");
+         disableScroll();
       }
       if(elem.closest('[data-id=callback]')){
          
          addClassElement(body, "lock");
          addClassElement(modalCallback, "visible");
+         disableScroll();
       }
       if(elem.closest(".modal__item-close") || elem.classList.contains("modal") || elem.classList.contains("modal__callback")){
          
